a0ca1232c5afd9d1abeb468886f18e518a7ea934
kayevirtucio89-hash <kayevirtucio89@gmail.com>
2025-08-10 20:28:10 +0800

    Assets

---
+++ Assets/SSSCRIPTS/PlayerController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody), typeof(BoxCollider))]
+public class NewBehaviourScript : MonoBehaviour
+{
+    [SerializeField] private Rigidbody _rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
